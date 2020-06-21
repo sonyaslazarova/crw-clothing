2983e171049641097959e9655038c4df105143b6
sonyalazarova <sonyaslazarova@gmail.com>
2020-06-21 21:21:02 +0300

    implementet cart reducer

---
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sign-in-sign-out.style.scss";
+import SignIn from '../../_components old/sign-in/sign-in.component';
+
+const SignInSignOutPage = () => (
+  <div className="sign-in-sign-up">
+    <SignIn/>
+  </div>
+)
+
+export default SignInSignOutPage;
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,6 @@
+import CartActionTypes from "./cart.types";
+
+export const toggleCartHidden = () => ({
+  type: CartActionTypes.TOOGLE_CART_HIDDEN,
+});
+
