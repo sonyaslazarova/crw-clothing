35dfea03d13fbf0ac805f4a8695f15bde3e72ed5
sonyalazarova <sonyaslazarova@gmail.com>
2020-06-22 13:18:07 +0300

    updated shop data, updated cart utils

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,7 @@ export const toggleCartHidden = () => ({
   type: CartActionTypes.TOOGLE_CART_HIDDEN,
 });
 
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
